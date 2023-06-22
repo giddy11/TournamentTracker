cee1448400e7115c03663432718e9280e1790790
gideon.edoghotu <gideon.edoghotu@CCLNG-PC505>
2023-06-22 11:40:51 +0100

    having a test file is mandatory

---
+++ TournamentTracker.UI/TournamentTracker.UI/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentTracker.UI.Models;
+
+namespace TournamentTracker.UI.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(_persons);
+        }
+
+        public IActionResult Create()
+        {
+            var person = new Person();
+            return View(person);
+        }
+
+        public IActionResult CreatePerson(Person person)
+        {
+            _persons.Add(person);
+            return RedirectToAction(nameof(Index));
+            //return View("Index");
+        }
+
+
+        private static List<Person> _persons = new List<Person>();
+    }
+}
